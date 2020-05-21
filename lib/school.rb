3d848e159d899ccba8a718586976dddb95cc9667
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-21 23:45:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+  end
+  
+  def school_name
+    @school_name
+  end
+
+  def roster
+  roster = {}
+  end
+  
+  def add_student(student_name, grade)
+  
+  
+  end
+
+    
+end
