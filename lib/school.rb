5a88426a231c10a6f6341f20394d51801d29722b
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-05-22 00:23:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -29,8 +29,8 @@ class School
   attr_accessor :roster
   
   def initialize(school_name)
-    @school_name = school_name
-   roster = {}
+    @name = school_name
+    @roster = {}
   end
   
   def add_student(name, grade)
